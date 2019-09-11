c80eeb0dc5097e3485953b4c8fee6afa83d4f33e
Montgomery Watts <mjw4633@rit.edu>
2019-09-11 00:21:34 -0400

    Modules separating resources by AWS Service

--- src/App.INF/src/main.tf
+++ src/App.INF/src/main.tf
@@ -7,13 +7,37 @@ provider "aws" {
 locals {
   table_name = "${var.environment}-${var.app_name}"
   ssm_prefix = "/${var.environment}/${var.app_name}/"
+  aliases    = ["${var.site_url}", "www.${var.site_url}"]
 }
 
 
-module "cdn" {
-  source = "./modules/cdn"
+module "s3" {
+  source = "./modules/s3"
   url    = "${var.site_url}"
 }
+module "acm" {
+  source           = "./modules/acm"
+  site_domain_name = "${var.site_url}"
+}
+
+module "route53" {
+  source                    = "./modules/route53"
+  domain_validation_options = "${module.acm.domain_validation_options}"
+  site_domain_name          = "${var.site_url}"
+  certificate_arn           = "${module.acm.certificate_arn}"
+  cloudfront_domain_name    = "${module.cloudfront.domain_name}"
+  cloudfront_hosted_zone_id = "${module.cloudfront.hosted_zone_id}"
+}
+
+
+module "cloudfront" {
+  source             = "./modules/cloudfront"
+  certificate_arn    = "${module.acm.certificate_arn}"
+  bucket_domain_name = "${module.s3.bucket_regional_domain_name}"
+  bucket_origin_id   = "S3-${var.site_url}"
+  aliases            = "${local.aliases}"
+}
+
 
 module "dynamodb" {
   source     = "./modules/dynamodb"
